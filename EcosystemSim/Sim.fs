1c3db24e25efc7dc0d26fc9ff00d99247ecd3f75
dhzdhd <ragas5432100@gmail.com>
2021-10-17 21:47:36 +0530

    Added collision support. Not finished yet

--- EcosystemSim/Sim.fs
+++ EcosystemSim/Sim.fs
@@ -67,31 +67,41 @@ module Sim =
             |> List.map (fun element ->
                 {element with Ticker = element.Ticker + 1})
             
-        let wallCollision (blob: Blob) =
-            if
-                blob.Center.X + blob.Radius > float32(Constants.WIDTH)
-                || blob.Center.X - blob.Radius < 0.f
-            then Vector2 (blob.Velocity.X * -1.f, blob.Velocity.Y)
-            elif
-                blob.Center.Y + blob.Radius > float32(Constants.HEIGHT)
-                || blob.Center.Y - blob.Radius < 0.f
-            then Vector2 (blob.Velocity.X, blob.Velocity.Y * -1.f)
-            else Vector2 ()
+        let checkForCollision =
+            blobList
+            |> List.map (fun (element: Blob) ->
+                foodList <- foodList |> List.map (fun (element': Food) ->
+                    let condition = Raylib.CheckCollisionCircles (element.Center, element.Radius, element'.Center, element'.Radius)
+                    // Do stuff here :)
+                    )   
+                )
             
-        let getColorAndLifetime (blob: Blob) =
-            match blob.Type with
-                | PassiveBlob -> ((Color.GREEN, float32(blob.Lifetime - float32(0.05)) / 100.f), blob.Lifetime - float32(0.05))
-                | AggroBlob -> ((Color.RED, float32(blob.Lifetime - 10.f) / 100.f), blob.Lifetime - 10.f)
-                | DiseasedBlob -> ((Color.YELLOW, float32(blob.Lifetime - 40.f) / 100.f), blob.Lifetime - 40.f)
-                
         let moveAndUpdateBlob list =
+            let wallCollision (blob: Blob) =
+                if
+                    blob.Center.X + blob.Radius > float32(Constants.WIDTH)
+                    || blob.Center.X - blob.Radius < 0.f
+                then Vector2 (blob.Velocity.X * -1.f, blob.Velocity.Y)
+                elif
+                    blob.Center.Y + blob.Radius > float32(Constants.HEIGHT)
+                    || blob.Center.Y - blob.Radius < 0.f
+                then Vector2 (blob.Velocity.X, blob.Velocity.Y * -1.f)
+                else Vector2 ()
+            
+            let getColorAndLifetime (blob: Blob) =
+                match blob.Type with
+                    | PassiveBlob -> ((Color.GREEN, float32(blob.Lifetime - 0.05f) / 100.f), blob.Lifetime - 0.05f)
+                    | AggroBlob -> ((Color.RED, float32(blob.Lifetime - 10.f) / 100.f), blob.Lifetime - 10.f)
+                    | DiseasedBlob -> ((Color.YELLOW, float32(blob.Lifetime - 40.f) / 100.f), blob.Lifetime - 40.f)
+
+            
             blobList <- list
                 |> List.map (fun (element: Blob) ->
                     let result = wallCollision element
                     
                     let vX, vY =
                         if result <> Vector2 () then result.X, result.Y
-                        elif element.Ticker % 60 = 0 then Utils.getRandomFloat(-3, 3), Utils.getRandomFloat(-3, 3) 
+                        elif element.Ticker % 60 = 0 then Utils.getRandomFloat(-1, 1), Utils.getRandomFloat(-1, 1) 
                         else element.Velocity.X, element.Velocity.Y
                     
                     let x = element.Center.X + vX
@@ -108,8 +118,8 @@ module Sim =
         let updateFood list =
             foodList <- list
                 |> List.map (fun (element: Food) ->
-                    let color = (Color.ORANGE, float32(element.Lifetime - float32(0.1) / 100.f))
-                    let lifetime = element.Lifetime - float32(0.1)
+                    let color = (Color.DARKGRAY, float32(element.Lifetime - 0.1f / 100.f))
+                    let lifetime = element.Lifetime - 0.1f
                     
                     {
                         element with
@@ -122,7 +132,7 @@ module Sim =
             let food = {
                 Center = Utils.getRandomVector (0, 1080, 0, 720)
                 Radius = 5.f
-                Color = Raylib.ColorAlpha (Color.ORANGE, float32(0.5))
+                Color = Raylib.ColorAlpha (Color.DARKGRAY, 0.0f)
                 Lifetime = 100.f
             }
             
@@ -135,21 +145,19 @@ module Sim =
     
     module private Blob =
         let initializeBlob () =
-            let blob = {
-                Center = Vector2(50.f, 50.f)
-                Radius = 10.f
-                Velocity = Utils.getRandomVector (-1, 1, -1, 1)
-                Color = Raylib.ColorAlpha (Color.GREEN, 1.f)
-                Type = BlobType.PassiveBlob
-                Lifetime = 100.f
-                Ticker = 0
-            }
-            blobList <- blob :: blobList
-            blobList <- {
-                blob with
-                    Velocity = Utils.getRandomVector (-1, 1, -1, 1)
+            let getBlob () =
+                {
                     Center = Utils.getRandomVector (0, Constants.WIDTH - 20, 0, Constants.HEIGHT - 20)
-            } :: blobList
+                    Radius = 10.f
+                    Velocity = Utils.getRandomVector (-1, 1, -1, 1)
+                    Color = Raylib.ColorAlpha (Color.GREEN, 1.f)
+                    Type = BlobType.PassiveBlob
+                    Lifetime = 100.f
+                    Ticker = 0
+                }
+            
+            for _ in 0 .. 5 do
+                blobList <- getBlob () :: blobList
             
         let createBlob blobType position=
             0
@@ -163,7 +171,7 @@ module Sim =
       
     let Setup () =
         // Update
-        if ticker % 100 = 0 then Food.createFood ();
+        if ticker % 100 = 0 then Food.createFood ()
         
         Update.updateTickerCounter ()
         Update.moveAndUpdateBlob blobList
