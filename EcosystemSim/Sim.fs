389933db1335bb5677ecc1971462d2430d339157
dhzdhd <ragas5432100@gmail.com>
2021-10-17 11:58:47 +0530

    Added a ticker and some other changes.

--- EcosystemSim/Sim.fs
+++ EcosystemSim/Sim.fs
@@ -3,7 +3,9 @@
 open System
 open System.Collections.Generic
 open System.Numerics
+open System.Timers
 open Microsoft.VisualBasic
+open Microsoft.VisualBasic.CompilerServices
 open Raylib_cs
 
 type BlobType =
@@ -18,6 +20,8 @@ type Blob =
         Velocity: Vector2
         Color: Color
         Type: BlobType
+        Lifetime: int
+        Ticker: int
     }
     
 type Food =
@@ -45,8 +49,17 @@ module private Utils =
 
 module Sim =
     let mutable blobList = List.empty
+//    let mutable foodList = List.empty
+    let mutable secondTicker = 0.
 
     module private Update =
+        let updateTickerCounter () =
+            secondTicker <- secondTicker + (1. / 60.)
+            
+            blobList <- blobList
+            |> List.map (fun element ->
+                {element with Ticker = element.Ticker + 1})
+            
         let wallCollision (blob: Blob) =
             if
                 blob.Center.X + blob.Radius > float32(Constants.WIDTH)
@@ -64,53 +77,63 @@ module Sim =
                     let result = wallCollision element
                     
                     let vX, vY =
-                        if result <> Vector2 () then result.X, result.Y else element.Velocity.X, element.Velocity.Y
+                        if result <> Vector2 () then result.X, result.Y
+                        elif element.Ticker % 60 = 0 then Utils.getRandomFloat(-3, 3), Utils.getRandomFloat(-3, 3) 
+                        else element.Velocity.X, element.Velocity.Y
                     
                     let x = element.Center.X + vX
                     let y = element.Center.Y + vY
-                        
                     {
                         element with
                             Center = Vector2 (x, y)
                             Velocity = Vector2 (vX, vY)
                     })
 
-    module private Draw =
+    module private Blob =
         let initializeBlob () =
             let blob = {
                 Center = Vector2(50.f, 50.f)
                 Radius = 10.f
-                Velocity = Utils.getRandomVector (-5, 5, -5, 5)
-                Color = Color.GREEN
+                Velocity = Utils.getRandomVector (-3, 3, -3, 3)
+                Color = Raylib.ColorAlpha (Color.GREEN, 1.f)
                 Type = BlobType.PassiveBlob
+                Lifetime = 100
+                Ticker = 0
             }
             blobList <- blob :: blobList
             blobList <- {
                 blob with
-                    Velocity = Utils.getRandomVector (-5, 5, -5, 5)
-                    Center = Utils.getRandomVector (0, 1080, 0, 720)
+                    Velocity = Utils.getRandomVector (-3, 3, -3, 3)
+                    Center = Utils.getRandomVector (0, Constants.WIDTH - 20, 0, Constants.HEIGHT - 20)
             } :: blobList
             
         let createBlob blobType position=
             0
         
         let drawBlobs () =
-            [blobList]
-                |> List.map (fun list ->
-                    list |> List.map (fun element -> 
-                        Raylib.DrawCircle (int(element.Center.X) ,int(element.Center.Y), element.Radius, element.Color)))
+            blobList
+            |> List.map (fun element -> 
+                Raylib.DrawCircle (int(element.Center.X) ,int(element.Center.Y), element.Radius, element.Color))
         
-    Draw.initializeBlob ()  
+    module private Food =
+        let drawFood () = ()
+//            foodList
+//            |> List.map (fun element ->
+//                Raylib.DrawCircle (int(element.Center.X)))
+        
+    Blob.initializeBlob ()  
       
     let Setup () =
         // Update
+        Update.updateTickerCounter ()
         Update.moveBlob blobList 
         
         // Draw
         Raylib.BeginDrawing ()
         Raylib.ClearBackground Color.BLACK
         
-        Draw.drawBlobs ()
+        Blob.drawBlobs ()
+//        Food.drawFood ()
                 
         Raylib.EndDrawing ()
         
